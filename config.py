2e66721ba691934f7d491e45c7373b221af0eb11
Harsh Kumar <harshkr53453@gmail.com>
2026-03-05 13:04:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # todo: Add your api key here
-api_key="3a5652b5-f196-4b1a-82aa-f5104b319a6c"
+api_key="Your open_Ai API key"
 
 # News API (Get free key from newsapi.org)
-news_api_key = "0bbfffcbbadb4db3a37f8065c607d376"
+news_api_key = "your news api key"
